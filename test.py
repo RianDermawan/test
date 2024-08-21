f77d5e8575a91235a820c458238e76d9b6b3db4a
riandermawan@bangunindo.com <jupyter@inst-builder-debian-11-build-build-ly0tq.us-central1-a.c.gce-image-builder.internal>
2024-08-21 14:59:29 +0000

    Updated File

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 # to make it easier to read multiple excel files, every file name excel must be in order such as data1, data2, data3 etc.  Because we use loop algorithm
 # example my directory folder is D:\\test, it folder have file such as laporan_iklim_harian (0), laporan_iklim_harian (1), ....  
 import pandas as pd
-path = 'D:\\test\\laporan_iklim_harian (' # file path
+path = 'D:\\test-lagi\\laporan_iklim_harian (' # file path
 isi = [] # empyt list
 
-# using loop for generate data to list 
-for i in range(12): # optional range, it depends on your data. My data have is 11 
-    data_i = pd.read_excel(path+str(i)+').xlsx') # read every excel
-    data_i = [data_i] # change type data to list
-    isi = isi + data_i  # combine all data into a list
-
+try:
+    # using loop for generate data to list 
+    for i in range(12): # optional range, it depends on your data. My data have is 11 
+        data_i = pd.read_excel(path+str(i)+').xlsx') # read every excel
+        data_i = [data_i] # change type data to list
+        isi = isi + data_i  # combine all data into a list
+except:
+    print('error')
+    
 # combine data into one file
 data_baru = pd.concat(isi, axis=0)
 
 # export excel 
-data_baru.to_excel('D:\\test\\data_baru.xlsx', index=False)
+data_baru.to_excel('D:\\test-lagi\\data_baru.xlsx', index=False)
